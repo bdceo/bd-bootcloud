5c09b6eb3d56cb1371fd420fb530e8862b3be45e
bdceo <bdceo@qq.com>
2017-02-18 21:07:16 +0800

    feign client add path on FeignClient Annotation

--- bd-eureka-provider/src/main/java/com/bdsoft/web/ProviderController.java
+++ bd-eureka-provider/src/main/java/com/bdsoft/web/ProviderController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/v1/provider")
 public class ProviderController {
 
 	private Logger log = LoggerFactory.getLogger(getClass());
--- bd-feign-consumer/src/main/java/com/bdsoft/service/ProviderClient.java
+++ bd-feign-consumer/src/main/java/com/bdsoft/service/ProviderClient.java
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 /**
  * provider的服务存根，同时指定服务提供者错误处理
  */
-@FeignClient(value = "bd-provider", fallback = ProviderClientHystrix.class)
+@FeignClient(value = "bd-provider",path="/v1/provider", fallback = ProviderClientHystrix.class)
 public interface ProviderClient {
 
-	@RequestMapping(value = "/append2", method = RequestMethod.GET)
+	@RequestMapping(value = "/append", method = RequestMethod.GET)
 	String append(@RequestParam("a") String a, @RequestParam("b") String b);
 
 }
