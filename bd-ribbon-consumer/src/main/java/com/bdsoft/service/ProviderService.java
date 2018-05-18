864bc2274b28f8fc1ef263cadca3d0b96f6c875d
Chenye Ding <dingchenye@juran.com.cn>
2018-05-18 18:20:36 +0800

    hystrix

--- bd-ribbon-consumer/src/main/java/com/bdsoft/service/ProviderService.java
+++ bd-ribbon-consumer/src/main/java/com/bdsoft/service/ProviderService.java
@@ -3,6 +3,8 @@ package com.bdsoft.service;
 import com.bdsoft.web.ConsumerController;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -10,18 +12,27 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class ProviderService {
 
+    private Logger log = LoggerFactory.getLogger(getClass());
     @Autowired
     RestTemplate template;
 
-    @HystrixCommand(fallbackMethod = "onProviderError", commandProperties = {
-            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000"),
-            @HystrixProperty(name = "metrics.rollingStats.timeInMilliseconds", value = "5000")
-    }, threadPoolProperties = {
-            @HystrixProperty(name = "coreSize", value = "1"),
-            @HystrixProperty(name = "maxQueueSize", value = "10")
-    })
+    @HystrixCommand(fallbackMethod = "onProviderError", commandKey = "appendKey",
+            commandProperties = {
+                    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000"),
+                    @HystrixProperty(name = "metrics.rollingStats.timeInMilliseconds", value = "5000")
+            },
+            // observableExecutionMode = ObservableExecutionMode.LAZY,
+            threadPoolProperties = {
+                    @HystrixProperty(name = "coreSize", value = "1"),
+                    @HystrixProperty(name = "maxQueueSize", value = "10")
+            })
     public String append() {
-        return template.getForEntity("http://" + ConsumerController.PROVIDER_SERVICE_ID + "/v1/provider/append?a=xx&b=oo", String.class).getBody();
+        Long start = System.currentTimeMillis();
+
+        String res = template.getForEntity("http://" + ConsumerController.PROVIDER_SERVICE_ID + "/v1/provider/append?a=To&b=From", String.class).getBody();
+
+        log.info("Spend time: {}", (System.currentTimeMillis() - start));
+        return res;
     }
 
     // 服务提供者错误时回调函数
